1599db3c436e134687e18e18fc0f5827d4534043
eldh <andreas.eldh@gmail.com>
2022-01-09 13:50:57 +0100

    Light theme

--- src/Square.tsx
+++ src/Square.tsx
@@ -13,7 +13,6 @@ export function Square(props: {
       className="center letter"
       style={{
         fontSize: "24px",
-        fontWeight: "600",
         textTransform: "uppercase",
         boxShadow:
           "0 2px 5px var(--shadow-mid), 0 1px 0 rgba(255, 255, 255, 0.15) inset, 0 -1px 0 rgba(0, 0, 0, 0.2) inset",
